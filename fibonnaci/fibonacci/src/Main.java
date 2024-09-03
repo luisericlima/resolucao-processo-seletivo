3fbca8af7e6c4d687bc1528ecc479c9a864bcd9b
luisericlima <luisericlima12@outlook.com>
2024-09-03 17:26:29 -0300

    Envio do arquivo

---
+++ fibonnaci/fibonacci/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+//        FIBONACCI
+//
+//        System.out.print("Qual termo deseja saber? ");
+//        int termo = input.nextInt();
+//
+//        int t1 = 0;
+//        int t2 = 1;
+//
+//        System.out.println(t1);
+//        System.out.println(t2);
+//
+//        int cont = 2;
+//
+//        while (cont < termo) {
+//            int t3 = t1 + t2;
+//            t1 = t2;
+//            t2 = t3;
+//            cont++;
+//            System.out.println(t3);
+//        }
+
+
+        //VERIFICAÇÃO DE CARACTERES
+//        System.out.print("Digite uma palavra: ");
+//        String palavra = input.next().toLowerCase();
+//
+//        System.out.print("Digite um caractere para contar: ");
+//        char caractere = input.next().toLowerCase().charAt(0);
+//
+//        int contador = 0;
+//
+//        for (int i = 0; i < palavra.length(); i++) {
+//            if (palavra.charAt(i) == caractere) {
+//                contador++;
+//            }
+//        }
+//
+//        System.out.printf("\nO caractere (%s) aparece (%d) vezes na palavra (%s)." ,caractere,  contador, palavra );
+    }
+}
